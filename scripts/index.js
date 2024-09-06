9525a435ecb90fe22049fa46c54988fd36a8412e
Mark Levy <levymark@gmail.com>
2024-09-07 01:18:48 +0300

    add index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,5 @@
-const initialCards = [];
+let initialCards = [];
+
 const yosemite = {
   name: "Yosemite Valley",
   link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
